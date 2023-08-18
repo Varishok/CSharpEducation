757f5d0b1cbbbfb85d1e2ab4c1bf928a916ab7b3
Varishok <varishok@gmail.com>
2023-08-18 16:34:08 +0400

    Adding abonents search by number and name.

--- Task3/Phonebook.cs
+++ Task3/Phonebook.cs
@@ -46,19 +46,46 @@
     public bool AddAbonent(string name, string phoneNumber)
     {
       var ab = new Abonent(name, phoneNumber);
-      abonents.Add(ab);
+      this.abonents.Add(ab);
       return true;
     }
 
+    /// <summary>
+    /// Delete a abonent by phone number.
+    /// </summary>
+    /// <param name="phoneNumber">Abonent's phone number.</param>
+    /// <returns>true - abonent deleted successfully. false - abonent can't deleted.</returns>
     public bool RemoveAbonent(string phoneNumber)
     {
-      var findAbonent = abonents.Find(abonent => abonent.GetPhoneNumber.Equals(phoneNumber));
+      var findAbonent = FindAbonentByPhone(phoneNumber);
       if (findAbonent != null)
       {
-        abonents.Remove(findAbonent);
+        this.abonents.Remove(findAbonent);
         return true;
       }
       return false;
+    }
+
+    /// <summary>
+    /// Find a abonent by phone number.
+    /// </summary>
+    /// <param name="phoneNumber">Abonent's phone number.</param>
+    /// <returns>Abonent if it was found, or null otherwise.</returns>
+    public Abonent? FindAbonentByPhone(string phoneNumber)
+    {
+      var findAbonent = this.abonents.Find(abonent => abonent.GetPhoneNumber.Equals(phoneNumber));
+      return findAbonent;
+    }
+
+    /// <summary>
+    /// Find a abonents by name.
+    /// </summary>
+    /// <param name="name">Abonent's name.</param>
+    /// <returns>A list of all abonents with the specified name.</returns>
+    public List<Abonent> FindAbonentsByName(string name)
+    {
+      var abonents = this.abonents.FindAll(abonents => abonents.GetName.Equals(name));
+      return abonents;
     }
   }
 }
