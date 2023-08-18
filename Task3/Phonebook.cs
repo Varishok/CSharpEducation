aa42925b7110da19ef8c5963c1551b2772469678
Varishok <varishok@gmail.com>
2023-08-18 13:22:40 +0400

    Singleton implementation of the class. List of abonent's. Adding and deleting abonent's.

--- Task3/Phonebook.cs
+++ Task3/Phonebook.cs
@@ -2,5 +2,63 @@
 {
   internal class Phonebook
   {
+    /// <summary>
+    /// Singleton field.
+    /// </summary>
+    private static Phonebook instance;
+
+    /// <summary>
+    /// List of abonents.
+    /// </summary>
+    private List<Abonent> abonents = new List<Abonent>();
+
+    /// <summary>
+    /// Get list of abonents.
+    /// </summary>
+    public List<Abonent> GetAbonents 
+    {
+      get { return abonents; }
+    }
+
+    /// <summary>
+    /// Init.
+    /// </summary>
+    private Phonebook() { }
+
+    /// <summary>
+    /// Singleton static init.
+    /// </summary>
+    /// <returns>Singleton object.</returns>
+    public static Phonebook GetInstance() 
+    { 
+      if (instance == null)
+      {
+        instance = new Phonebook();
+      }
+      return instance; 
+    }
+
+    /// <summary>
+    /// Add abonent in list.
+    /// </summary>
+    /// <param name="name">Abonent's name.</param>
+    /// <param name="phoneNumber">Abonent's phone number.</param>
+    public bool AddAbonent(string name, string phoneNumber)
+    {
+      var ab = new Abonent(name, phoneNumber);
+      abonents.Add(ab);
+      return true;
+    }
+
+    public bool RemoveAbonent(string phoneNumber)
+    {
+      var findAbonent = abonents.Find(abonent => abonent.GetPhoneNumber.Equals(phoneNumber));
+      if (findAbonent != null)
+      {
+        abonents.Remove(findAbonent);
+        return true;
+      }
+      return false;
+    }
   }
 }
