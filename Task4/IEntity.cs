d54e2a6c7789da44c9aa3b4f94ee21b2efbd32ec
Varishok <varishok@gmail.com>
2023-09-10 19:58:43 +0400

    Создание класса для сущности. Создание интерфейса для операций с сущностями.

---
+++ Task4/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Task4
+{
+  /// <summary>
+  /// Сущность.
+  /// </summary>
+  /// <typeparam name="T">Тип сущности.</typeparam>
+  internal class IEntity<T>
+  {
+    /// <summary>
+    /// Уникальный идентификатор сущности.
+    /// </summary>
+    public T Id { get; set; }
+  }
+}
