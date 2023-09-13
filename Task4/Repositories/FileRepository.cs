ffca7d1b1c6eb9257a1bed8f82f686c35187298c
Varishok <varishok@gmail.com>
2023-09-13 21:51:59 +0400

    Ошибка в поиске.

--- Task4/Repositories/FileRepository.cs
+++ Task4/Repositories/FileRepository.cs
@@ -39,7 +39,7 @@ namespace Task4.Repositories
 
     public void UpdateEntity(T entity)
     {
-      T updatingEntity = Files.Where(x => x.Id == entity.Id);
+      T updatingEntity = Files.Where(x => x.Id == entity.Id).FirstOrDefault();
       File.Move(updatingEntity.FileInfo.FullName, entity.FileInfo.FullName);
       updatingEntity = entity;
     }
