9f11fec627eda0374e322032f72a9665bf67258b
Varishok <varishok@gmail.com>
2023-09-13 21:57:34 +0400

    Добавления сущности и репозитория для работы в памяти.

---
+++ Task4/Entities/MemoryEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Task4.Entities
+{
+  /// <summary>
+  /// Сущность в памяти.
+  /// </summary>
+  public class MemoryEntity : IEntity
+  {
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public MemoryEntity() 
+    { 
+      this.Id = Guid.NewGuid();
+    }
+  }
+}
