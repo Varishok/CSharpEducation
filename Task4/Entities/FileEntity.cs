7521c0304ac33ccbeae08fbee924ba3d91da8a72
Varishok <varishok@gmail.com>
2023-09-13 20:46:38 +0400

    Добавления сущности и репозитория для файлов.

---
+++ Task4/Entities/FileEntity.cs
@@ -0,0 +1,24 @@
+﻿namespace Task4
+{
+  /// <summary>
+  /// Файловая сущность.
+  /// </summary>
+  internal class FileEntity : IEntity
+  {
+    public Guid Id { get; }
+
+    /// <summary>
+    /// Информация о файле.
+    /// </summary>
+    public FileInfo FileInfo { get; set; }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public FileEntity(FileInfo fileInfo)
+    {
+      Id = Guid.NewGuid();
+      FileInfo = fileInfo;
+    }
+  }
+}
---
+++ Task4/Repositories/FileRepository.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+
+namespace Task4
+{
+  /// <summary>
+  /// Репозиторий файлов.
+  /// </summary>
+  /// <typeparam name="T">Файловые сущности.</typeparam>
+  internal class FileRepository<T> : IRepository<T> where T : FileEntity
+  {
+    /// <summary>
+    /// Список файлов репозитория.
+    /// </summary>
+    public List<T> Files { get; set; }
+
+    public void CreateEntity(T entity)
+    {
+      using (FileStream fs = File.Create(entity.FileInfo.FullName))
+      {
+        this.Files.Add(entity);
+      }
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+      return Files;
+    }
+
+    public T GetEntity(int id)
+    {
+      return this.Files[id];
+    }
+
+    public void UpdateEntity(T entity)
+    {
+      T updatingEntity = Files.Where(x => x.Id == entity.Id).FirstOrDefault();
+      updatingEntity = entity;
+    }
+
+    public void DeleteEntity(int id)
+    {
+      T entity = this.Files[id];
+      Files.Remove(entity);
+    }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="path">Путь к папке.</param>
+    public FileRepository(string path) 
+    {
+      var directory = new DirectoryInfo(path);
+      Files = new List<T>();
+
+      foreach(FileInfo file in directory.GetFiles())
+      {
+        T entity = (T)new FileEntity(file);
+        Files.Add(entity);
+      }
+    }
+  }
+}
