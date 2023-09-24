d791ef7da5b0bb7d41094f453a59fc1368f9873d
Varishok <fktrc444@mail.ru>
2023-09-24 15:53:42 +0400

    Тесты валидации списка телефонных номеров.

--- Task6/PhonebookTests/PhoneNumberValidatorTests.cs
+++ Task6/PhonebookTests/PhoneNumberValidatorTests.cs
@@ -8,12 +8,14 @@ namespace TestPhonebook
   public class PhoneNumberValidatorTests
   {
     private PhoneNumber validPhoneNumber;
+    private PhoneNumber validPhoneNumber2;
     private PhoneNumber invalidPhoneNumber;
 
     [SetUp]
     public void Setup()
     {
       validPhoneNumber = new PhoneNumber("+7(999)999-9999", PhoneNumberType.Personal);
+      validPhoneNumber2 = new PhoneNumber("+7(888)888-8888", PhoneNumberType.Work);
       invalidPhoneNumber = new PhoneNumber("abc", PhoneNumberType.Personal);
     }
 
@@ -24,9 +26,32 @@ namespace TestPhonebook
     }
 
     [Test]
+    public void PhoneNumberValidator_ValidateList_SuccessfullyValidate()
+    {
+      List<PhoneNumber> listValidNumber = new List<PhoneNumber>()
+      {
+        validPhoneNumber,
+        validPhoneNumber2,
+      };
+      Assert.DoesNotThrow(() => PhoneNumberValidator.ValidateList(listValidNumber));
+    }
+
+    [Test]
     public void PhoneNumberValidator_Validate_ThrowsExcpetion()
     {
       Assert.Throws<ArgumentException>(() => PhoneNumberValidator.Validate(invalidPhoneNumber));
+    }
+
+    [Test]
+    public void PhoneNumberValidator_ValidateList_ThrowsExcpetion()
+    {
+      List<PhoneNumber> listWithInvalidNumber = new List<PhoneNumber>()
+      {
+        validPhoneNumber,
+        validPhoneNumber2,
+        invalidPhoneNumber
+      };
+      Assert.Throws<ArgumentException>(() => PhoneNumberValidator.ValidateList(listWithInvalidNumber));
     }
   }
 }
