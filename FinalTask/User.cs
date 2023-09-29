7d29e367c85d17ff981d717b8382d56aeb487c72
Varishok <varishok@gmail.com>
2023-09-29 17:39:12 +0400

    Класс пользователя. Добить свойства. Добавить конструкторы.

---
+++ FinalTask/User.cs
@@ -0,0 +1,43 @@
+﻿namespace FinalTask
+{
+  /// <summary>
+  /// Пользователь библиотеки.
+  /// </summary>
+  internal class User
+  {
+    #region Поля и свойства
+
+    /// <summary>
+    /// Ид.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Имя.
+    /// </summary>
+    public string Name { get; set; }
+
+    #endregion
+
+    #region Конструкторы
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="name">Имя пользователя.</param>
+    public User(string name) : this(Guid.NewGuid(), name) { }
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="id">Ид пользователя.</param>
+    /// <param name="name">Имя пользователя.</param>
+    public User(Guid id, string name)
+    {
+      this.Id = id;
+      this.Name = name;
+    }
+
+    #endregion
+  }
+}
